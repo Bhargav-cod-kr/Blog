7774ad1055cc4a63c8c8d08012a1646f1942c552
Bhargav Yadav <bhargavyadav145@gmail.com>
2025-10-31 10:30:56 +0530

    Refactor Vite configuration for consistency and clarity

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// __define-ocg__ Set correct base path for GitHub Pages
 export default defineConfig({
-  base: "/Blog/",
   plugins: [react()],
-});
+  base: '/Blog/', // <-- this must match your repo name exactly
+})
